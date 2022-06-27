7086c7f164f84fcb63e7259df71074d7d2999c8b
Baulin_Roman_Sergeevi4 <poma.baulin@mail.ru>
2022-06-27 23:01:09 +0300

    DZ

---
+++ Task_10/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Write("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+string numtext = Convert.ToString(num);
+
+if (numtext.Length >2)
+    {
+        Console.WriteLine("Третия цифра :" + numtext[2]);
+    }
+else 
+    {
+        Console.WriteLine("Третьей цифры нет");
+    }
---
+++ Task_15/Program.cs
@@ -0,0 +1,14 @@
+﻿ // Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.WriteLine("Введите цифру:");
+int cifra = Convert.ToInt32(Console.ReadLine());
+
+if(cifra == 6 || cifra == 7)
+    {
+        Console.Write("Выходной");
+    }
+    if (cifra>=1 && cifra<=5)
+        {
+            Console.Write("Будни");
+        }
+    
