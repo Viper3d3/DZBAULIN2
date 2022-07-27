609d8e2a7da17b856fd0ab13c92a37e83727ed2f
Baulin_Roman_Sergeevi4 <poma.baulin@mail.ru>
2022-07-28 01:32:48 +0300

    Hello

--- taks7_2/Program.cs
+++ taks7_2/Program.cs
@@ -7,33 +7,66 @@
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 
 
-int [,] myArray = 
+// int [,] myArray = 
+// {
+//     {1,4,7,2},
+//     {5,9,2,3},
+//     {8,4,2,4}
+// };
+
+// for (int i = 0; i < myArray.GetLength(0); i++)
+// {
+//     for (int j = 0; j < myArray.GetLength(1); j++)
+//     {
+//         Console.Write(myArray[i,j]+ "\t");
+//     }
+//      Console.WriteLine();     
+// }
+
+
+
+
+
+int[,] myArray = 
+
 {
+
     {1,4,7,2},
+
     {5,9,2,3},
+
     {8,4,2,4}
+
 };
 
+  
+
 for (int i = 0; i < myArray.GetLength(0); i++)
+
 {
+
     for (int j = 0; j < myArray.GetLength(1); j++)
+
     {
-        Console.Write(myArray[i,j]+ "\t");
+
+        Console.Write(myArray[i, j] + "\t");
+
     }
-     Console.WriteLine();     
+
+    Console.WriteLine();
+
 }
 
+  
 
 for (int j = 0; j < myArray.GetLength(1); j++)
 {
-    double sumArifmetic = 0;
+    double Arifmeticsum  = 0;
     for (int i = 0; i < myArray.GetLength(0); i++)
     {
-        sumArifmetic += myArray[i, j];
+        Arifmeticsum  += myArray[i, j];
     }
     
-    Console.Write(Math.Round(sumArifmetic / myArray.GetLength(0),1)+"\t");
+    Console.Write(Math.Round(Arifmeticsum  / myArray.GetLength(0),1)+"\t");
 } 
-
-
-
+  
